a22b8cfacf47bf6abeb9cddf90b0bdecf9689e2d
miya10kei <miya10kei@gmail.com>
2021-07-09 14:49:49 +0900

    :bug: Support BSD sed

--- conf.d/git_fish.fish
+++ conf.d/git_fish.fish
@@ -1,7 +1,7 @@
 function __git_fish_install --on-event git_fish_install
   set -l complet_filepath $HOME/.config/fish/completions/git_fish.fish
   if not test -e $complet_filepath
-    cat /usr/share/fish/completions/git.fish | sed -r 's/(complete.*) git (.+)/\1 git_fish \2/g'    >  $complet_filepath
+    cat /usr/share/fish/completions/git.fish | sed -E 's/(complete.*) git (.+)/\1 git_fish \2/g'    >  $complet_filepath
     echo "complete -f -c git_fish -n '__fish_use_subcommand' -a 'cd'     -d 'Change git directory'" >> $complet_filepath
     echo "complete -f -c git_fish -n '__fish_use_subcommand' -a 'erase'  -d 'Erase branches'"       >> $complet_filepath
     echo "complete -f -c git_fish -n '__fish_use_subcommand' -a 'select' -d 'Select branch'"        >> $complet_filepath
